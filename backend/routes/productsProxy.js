a2901766d1a72b6a0c9753bba9f22abd3708bfaf
ebolarium <barisboga@gmail.com>
2024-10-30 23:01:54 +0300

    Paddle Sandbox Impl.13

--- backend/routes/productsProxy.js
+++ backend/routes/productsProxy.js
@@ -5,35 +5,34 @@ const router = express.Router();
 // productsProxy.js
 router.get('/products', async (req, res) => {
     try {
-      const response = await fetch('https://sandbox-api.paddle.com/products', {
+      const response = await fetch('https://sandbox-api.paddle.com/prices', {
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer 0ca5518f6c92283bb2600c0e9e2a967376935e0566a4676a19', // Özel token bilgisi
-          'Vendor-Id': '24248'
-        }
+          'Authorization': 'Bearer 0ca5518f6c92283bb2600c0e9e2a967376935e0566a4676a19',
+          'Vendor-Id': '24248',
+        },
       });
   
       if (!response.ok) {
+        const errorText = await response.text();
+        console.error('Failed to fetch products from Paddle API:', errorText);
         return res.status(response.status).send('Failed to fetch products from Paddle API');
       }
   
       const data = await response.json();
-      const products = data.data.map(item => ({
-        id: item.data.id,
-        name: item.data.name,
-        description: item.data.description,
-        price: item.data.unit_price.amount,
-        currency: item.data.unit_price.currency_code,
-        interval: item.data.billing_cycle.interval,
-        frequency: item.data.billing_cycle.frequency
-      }));
-  
-      res.json(products);
+      res.json(data.data.map(item => ({
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        price: item.unit_price.amount,
+        currency: item.unit_price.currency_code,
+        interval: item.billing_cycle.interval,
+        frequency: item.billing_cycle.frequency,
+      })));
     } catch (error) {
       console.error('Error fetching products from Paddle API:', error);
       res.status(500).send('Internal Server Error');
     }
   });
-
   
   module.exports = router;
