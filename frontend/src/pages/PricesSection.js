8a4472281cbaac13e908f95fd4637252aa8e7f7c
ebolarium <barisboga@gmail.com>
2024-11-02 02:11:07 +0300

    PriceSection Test 2

--- frontend/src/pages/PricesSection.js
+++ frontend/src/pages/PricesSection.js
@@ -1,8 +1,19 @@
 // PricesSection.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box, Typography, Button } from '@mui/material';
 
+// Paddle tanımını global değişken olarak kullanıyoruz
+/* global Paddle */
+
 const PricesSection = () => {
+  useEffect(() => {
+    // Paddle'ı başlat ve sadece client-side token kullanarak yapılandır
+    Paddle.Environment.set("sandbox");
+    Paddle.Initialize({
+      token: "test_605824494b6e720104d54646e1c" // Buraya kendi client-side token'ınızı ekleyin
+    });
+  }, []);
+
   const handleSubscribe = () => {
     // Paddle entegrasyonu: checkout penceresini aç
     Paddle.Checkout.open({
